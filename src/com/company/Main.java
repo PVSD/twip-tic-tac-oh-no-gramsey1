d7eafcd227d51f3a52c5655927b497b560ba0c04
gramsey1 <graceramsey312@yahoo.com>
2018-12-21 11:11:46 -0500

    edennnnnnn

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,20 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Below is the board you will be using for this game of tic tac toe \n _ | _ | _ \n _ | _ | _ \n   |   |");
-        int [] powerglide = new int[100];
-        
-
+        System.out.println("Below is the board you will be using for this game of tic tac toe \n 1 | 2 | 3 \n 4 | 5 | 6 \n 7 | 8 | 9");
+        int[][] powerglide = new int[100][];
+        System.out.println("Player one: Where would you like to place your first X?" + " Choose one of the numbers on the board to place where you want to go");
+        Scanner scan = new Scanner(System.in);
+        String control = scan.nextLine();
+        while (control.equalsIgnoreCase("1")) {
+            StringBuffer string = new StringBuffer();
+            String s = "X";
+            string.append("\n " + s + " | 2 | 3 \n 4 | 5 | 6 \n 7 | 8 | 9");
+            String ok = string.toString();
+            System.out.println("Here is your board so far" + ok);
+            System.out.println("Player two: Where would you like to place your first O?" + " Choose one of the numbers on the board to place where you want to go");
+            Scanner kbInput1 = new Scanner(System.in);
+            String O = kbInput1.nextLine();
+        }
     }
 }
